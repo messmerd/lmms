e86dc5945e80cd804a749efad128373fc91096d6
Dalton Messmer <messmer.dalton@gmail.com>
2024-05-28 17:40:43 -0400

    Test

--- cmake/toolchains/Ubuntu-MinGW-W64-32.cmake
+++ cmake/toolchains/Ubuntu-MinGW-W64-32.cmake
@@ -1,3 +1,4 @@
-INCLUDE(${CMAKE_CURRENT_LIST_DIR}/common/Win32.cmake)
+
+#INCLUDE(${CMAKE_CURRENT_LIST_DIR}/common/Win32.cmake)
 INCLUDE(${CMAKE_CURRENT_LIST_DIR}/common/Ubuntu-MinGW-W64.cmake)
 INCLUDE($ENV{VCPKG_INSTALLATION_ROOT}/scripts/buildsystems/vcpkg.cmake)
